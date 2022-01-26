e32b023a1160c932639f9d93c9c62e70db81f880
Glevinzon Dapal <glevinzond@gmail.com>
2022-01-26 10:58:49 +0800

    chore: initial files for svelte kit + houdini

---
+++ jamclout-svelte/svelte.config.js
@@ -0,0 +1,33 @@
+import adapter from '@sveltejs/adapter-auto';
+import houdini from 'houdini-preprocess';
+import path from 'path';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(houdini()),
+
+	kit: {
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		},
+
+		vite: {
+			resolve: {
+				alias: {
+					$houdini: path.resolve('.', '$houdini')
+				}
+			}
+		}
+	}
+};
+
+export default config;
