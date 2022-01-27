34ab8a2c4832acb94f42db25726addeef1aacb99
Glevinzon Dapal <glevinzond@gmail.com>
2022-01-27 18:24:57 +0800

    chore: add redirects to blank routes

--- jamclout-svelte/src/routes/u/[username]/index.svelte
+++ jamclout-svelte/src/routes/u/[username]/index.svelte
@@ -1,52 +1,8 @@
-<script lang="ts" context="module">
-	import { client } from '$lib/client';
-	import type { Load } from '@sveltejs/kit';
-	import { GET_CREATOR_POSTS_WITHOUT_AUTH } from '../../../graphql/queries/creator';
-
-	let creatorProfile;
-
-	// see https://kit.svelte.dev/docs#loading
-	export const load: Load = async ({ params }) => {
-		const creatorUsername = params.username;
-
-		creatorProfile = await client.query(GET_CREATOR_POSTS_WITHOUT_AUTH, {
-			variables: {
-				username: creatorUsername
-			},
-			fetchPolicy: 'no-cache'
-		});
-
+<script context="module">
+	export async function load({ params }) {
 		return {
-			props: { creatorProps: creatorProfile, creatorUname: creatorUsername }
+			status: 302,
+			redirect: `${params.username}/posts`
 		};
-	};
+	}
 </script>
-
-<script lang="ts">
-	export let creatorProps;
-	console.log('🚀 ~ file: index.svelte ~ line 33 ~ creatorProps', creatorProps);
-	export let creatorUname;
-	console.log('🚀 ~ file: index.svelte ~ line 34 ~ creatorUname', creatorUname);
-</script>
-
-{#if creatorProps}
-	{#if $creatorProps.loading}
-		Loading... {creatorUname}
-	{:else if $creatorProps.error}
-		Error: {$creatorProps.error.message}
-	{:else}
-		<ul>
-			{#if $creatorProps.data && $creatorProps.data.creatorAccount}
-				<!-- {console.log($creatorProps.data.creatorAccount.posts.edges)} -->
-				{#each $creatorProps.data.creatorAccount.posts.edges as node}
-					<li>
-						{node.node.id}
-					</li>
-				{/each}
-			{/if}
-		</ul>
-	{/if}
-{/if}
-
-<style>
-</style>
