519efcd78257ab3f38565876cb00714a43610cba
Glevinzon Dapal <glevinzond@gmail.com>
2022-02-25 15:18:18 +0800

    chore: msw integration

--- 008/src/__tests__/Welcome.test.ts
+++ 008/src/__tests__/Welcome.test.ts
@@ -2,13 +2,15 @@
  * @jest-environment jsdom
  */
 
- import "@testing-library/jest-dom";
- import Layout from "../routes/__layout.svelte";
- import { render } from "@testing-library/svelte";
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/svelte';
+import Layout from '../routes/__layout.svelte';
 
- describe("Test if Jest is working", () => {
-    test("Welcome", () => {
-      const { getByText } = render(Layout);
-      expect(getByText("HOV Labs - Svelte with TDD")).toBeInTheDocument();
-    });
-  });
+describe('Landing Page', () => {
+	describe('Layout', () => {
+		it('shows the heading', () => {
+			const { getByText } = render(Layout);
+			expect(getByText('HOV Labs - Svelte with TDD')).toBeInTheDocument();
+		});
+	});
+});
