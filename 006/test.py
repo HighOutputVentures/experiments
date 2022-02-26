017f85a1f9d8302984c6487beb54eaea226d8824
ccpacillos <clint.pacillos@gmail.com>
2022-02-27 00:34:03 +0800

    feat(006): remove test.py

--- 006/test.py
+++
@@ -1,7 +0,0 @@
-import os
-os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.6/bin")
-
-import keras
-import tensorflow as tf
-tf.test.gpu_device_name()
-tf.config.list_physical_devices('GPU')
