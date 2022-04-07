8dad54d3bb3486b79cb1a9c8981d79758f196dd8
rogermadjos <roger@hov.co>
2022-04-07 09:28:59 +0800

    feat(012): add example

--- 012/src/index.ts
+++ 012/src/index.ts
@@ -1,3 +1,12 @@
-export * from './Node';
-export * from './Editor';
-export * from './TextNode';
+import { Editor, NodeType } from './Editor';
+import { TextNode } from './TextNode';
+
+export {
+  Editor,
+  NodeType,
+  TextNode
+}
+
+const editor = document.querySelector("#editor");
+const node = new TextNode("**bold** *italic* ~~strike through~~ ***bold and italic*** **bold and nested _italic_** ");
+editor.appendChild(node.getHTMLElement());
