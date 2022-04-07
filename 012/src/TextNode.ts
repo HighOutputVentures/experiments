8d0f8c9e31c7caf61c48bbdde1b78d2e14a257c9
pailadin <21221101+pailadin@users.noreply.github.com>
2022-04-07 15:31:26 +0800

    feat(012): added quotes

--- 012/src/TextNode.ts
+++ 012/src/TextNode.ts
@@ -8,21 +8,38 @@ export class TextNode extends Node {
 
     this.elem = document.createElement("div");
     this.elem.contentEditable = "true";
+    this.elem.className = "text-node";
     this.elem.innerText = data;
   }
 
   public getHTMLElement(): HTMLElement {
     let text = this.elem.innerText;
 
+    // Check if a quote:
+    let isAQuote = false;
+    if (text.startsWith(">")) {
+      isAQuote = true;
+      text = text.slice(1);
+    }
+
+    // Apply bold:
     text = text.replace(/\*{2}([^\*]+)\*{2}/g, "<span class='bold'>$1</span>");
     text = text.replace(/_{2}([^_]+)_{2}/g, "<span class='bold'>$1</span>");
 
+    // Apply italic:
     text = text.replace(/\*([^\*]+)\*/g, "<span class='italic'>$1</span>");
     text = text.replace(/_([^_]+)_/g, "<span class='italic'>$1</span>");
 
+    // Apply strikethrough:
     text = text.replace(/~~([^~]+)~~/g, "<span class='strikethrough'>$1</span>");
 
+    // Apply code:
     text = text.replace(/`([^\`]+)`/g, "<span class='code'>$1</span>");
+
+    // Apply quote (if needed):
+    if (isAQuote) {
+      text = `<span class='quote'>${text}</span>`;
+    }
 
     this.elem.innerHTML = text;
     return this.elem;
