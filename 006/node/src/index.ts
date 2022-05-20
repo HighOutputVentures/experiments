28b5bfa6168007d86965750ace87d9380d042e6c
ccpacillos <clint.pacillos@gmail.com>
2022-05-20 11:15:02 +0800

    feat(006): add http server

--- 006/node/src/index.ts
+++ 006/node/src/index.ts
@@ -1,5 +1,14 @@
 import { CronJob } from 'cron';
+import Koa from 'koa';
 import detect from './detect';
+
+const app = new Koa();
+
+app.use((ctx) => {
+  ctx.body = 'Wallet System Anomaly Detection';
+});
+
+app.listen(process.env.PORT);
 
 const job = new CronJob('*/1 * * * *', detect);
 
