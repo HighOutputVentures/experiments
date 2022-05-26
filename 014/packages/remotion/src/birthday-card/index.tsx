fe2faa021193aeae0546d7af77a3012b3e4600f6
JP Calvo <calvojp92@gmail.com>
2022-05-27 00:51:12 +0800

    slide 2 partial

--- 014/packages/remotion/src/birthday-card/index.tsx
+++ 014/packages/remotion/src/birthday-card/index.tsx
@@ -281,10 +281,53 @@ const Scribbles = () => {
 const Slide2 = () => {
 	return (
 		<Sequence from={300}>
-			<div>Greetings</div>
+			<AbsoluteFill
+				style={{
+					backgroundColor: '#fff',
+					color: '#000',
+					padding: 64,
+					alignItems: 'center',
+				}}
+			>
+				<Slide2Logo>
+					<Img
+						src={staticFile('hov-logo-dark.png')}
+						height={80}
+						width={80}
+						alt=""
+					/>
+				</Slide2Logo>
+
+				<Slide2Main>
+					<Card>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio sit
+						pariatur vel quae eligendi ex.
+					</Card>
+				</Slide2Main>
+			</AbsoluteFill>
 		</Sequence>
 	);
 };
+
+const Card = styled.div`
+	color: #fff;
+	background-color: #1a1d21;
+	font-size: 2.8rem;
+	padding: 2rem;
+	border-radius: 1rem;
+`;
+
+const Slide2Main = styled.main`
+	padding: 1rem;
+	flex-grow: 1;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
+
+const Slide2Logo = styled.div`
+	margin: 0 auto;
+`;
 
 const Selfie = styled.div`
 	margin-top: 4rem;
