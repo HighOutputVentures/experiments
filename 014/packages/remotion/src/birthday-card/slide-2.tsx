23e3b215edf82e838191d59bffc93e4398148c53
JP Calvo <calvojp92@gmail.com>
2022-05-27 12:48:40 +0800

    updated texts

--- 014/packages/remotion/src/birthday-card/slide-2.tsx
+++ 014/packages/remotion/src/birthday-card/slide-2.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-danger */
 import {
 	AbsoluteFill,
 	Img,
@@ -7,6 +8,7 @@ import {
 	useCurrentFrame,
 } from 'remotion';
 import styled from 'styled-components';
+import {v4 as uuid} from 'uuid';
 
 export const Slide2 = () => {
 	return (
@@ -36,36 +38,79 @@ export const Slide2 = () => {
 	);
 };
 
+const messages = [
+	{
+		from: 'estella',
+		date: '8:53 AM',
+		message:
+			"<p>Happy Birthday Jelo! You've been a great addition to the team! Enjoy your day. And to many more birthday celebrations with us here at HOV! Tara coffee! <3</p> ~ yours truly, Ate E.",
+	},
+	{
+		from: 'jynn',
+		date: '8:53 AM',
+		message:
+			'Happy birthday! i wish you all the best and thanks for always being there 🙌 To more birthdays and blessings Sensei 😊',
+	},
+	{
+		from: 'Arriele',
+		date: '8:53 AM',
+		message:
+			'Happy Birthday Jelo! For an intern you have done such a great job, so keep it up! God bless always and more more birthdays to come <3',
+	},
+	{
+		from: 'Lord Adrian Tisbe',
+		date: '8:53 AM',
+		message: 'Happiest Birthday Senpapi. Salamat po sa pa carry.',
+	},
+	{
+		from: 'Lord Adrian Tisbe',
+		date: '8:53 AM',
+		message: 'Happiest Birthday Senpapi. Salamat po sa pa carry.',
+	},
+	{
+		from: 'Jade',
+		date: '8:53 AM',
+		message:
+			'Wishing you a fantastic birthday and a wonderful year ahead. Make every day count. Happy Birthday, Jelo!',
+	},
+];
+
 const Messages = () => {
 	const frame = useCurrentFrame();
 
-	const left = interpolate(frame, [0, 30], [-100, 10], {
-		extrapolateLeft: 'clamp',
-		extrapolateRight: 'clamp',
-	});
-
-	const right = interpolate(frame, [60, 90], [10, -100], {
-		extrapolateLeft: 'clamp',
-		extrapolateRight: 'clamp',
-	});
-
 	return (
 		<>
-			<Card
-				style={{
-					left: frame <= 40 ? left + '%' : undefined,
-					right: right + '%',
-				}}
-			>
-				<CardHeader>
-					Greetings from <em>@estella</em> • Today at 8:53 AM
-				</CardHeader>
+			{[messages[0]].map(({from, message, date}) => {
+				const left = interpolate(frame, [0, 30], [-100, 10], {
+					extrapolateLeft: 'clamp',
+					extrapolateRight: 'clamp',
+				});
 
-				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio sit
-					pariatur vel quae eligendi ex.
-				</p>
-			</Card>
+				const right = interpolate(frame, [30, 60], [10, -100], {
+					extrapolateLeft: 'clamp',
+					extrapolateRight: 'clamp',
+				});
+
+				return (
+					<Card
+						key={uuid()}
+						style={{
+							left: frame <= 40 ? left + '%' : undefined,
+							right: right + '%',
+						}}
+					>
+						<CardHeader>
+							Greetings from <em>@{from}</em> • Today at {date}
+						</CardHeader>
+
+						<p
+							dangerouslySetInnerHTML={{
+								__html: message,
+							}}
+						/>
+					</Card>
+				);
+			})}
 		</>
 	);
 };
