add1f38d64cc244d38160a381ee4a6e4111a9992
JP Calvo <calvojp92@gmail.com>
2022-05-30 21:21:43 +0800

    updates

--- 014/packages/remotion-plugins/src/pages/index.tsx
+++ 014/packages/remotion-plugins/src/pages/index.tsx
@@ -4,17 +4,30 @@ export default function Index() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center font-mono text-lg">
       <nav>
-        <ul>
-          <li>
-            <Link
-              to="/plugins/video"
-              className="rounded-md border border-zinc-600 p-4 outline-none"
-            >
-              @remotion/video
-            </Link>
-          </li>
+        <ul className="flex gap-4">
+          {plugins.map(({name, url}) => (
+            <li key={name}>
+              <Link
+                to={url}
+                className="rounded-md border border-zinc-600 p-4 outline-none"
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
   );
 }
+
+const plugins = [
+  {
+    url: "/plugins/video",
+    name: "@remotion/video",
+  },
+  {
+    url: "/plugins/three",
+    name: "@remotion/three",
+  },
+];
