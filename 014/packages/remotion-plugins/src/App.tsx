5d3f660b1e4d892d1d4a1715ae049efa8a4a312a
JP Calvo <calvojp92@gmail.com>
2022-05-30 21:21:29 +0800

    added 404

--- 014/packages/remotion-plugins/src/App.tsx
+++ 014/packages/remotion-plugins/src/App.tsx
@@ -1,15 +1,17 @@
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import "./index.css";
-import Index from "./pages";
+import Index from "./pages/index";
+import NotFound from "./pages/notfound";
 import RemotionVideo from "./pages/video";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Navigate to="/plugins" replace />} />
         <Route path="/plugins" element={<Index />} />
         <Route path="/plugins/video" element={<RemotionVideo />} />
-        <Route path="*" element={<Navigate to="/plugins" replace />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ 014/packages/remotion-plugins/src/pages/notfound.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-black text-zinc-800">
+      <h1 className="text-9xl">404</h1>
+    </div>
+  );
+}
