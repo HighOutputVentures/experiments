0857a85af1eee85d57023d90e7379b603d6fbb56
JP Calvo <calvojp92@gmail.com>
2022-06-03 19:41:48 +0800

    lint'd

--- 014/packages/remotion-plugins/src/pages/_document.tsx
+++ 014/packages/remotion-plugins/src/pages/_document.tsx
@@ -5,7 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Cardo:wght@400;700&family=Inter:wght@400;700;900&display=swap"
           rel="stylesheet"
