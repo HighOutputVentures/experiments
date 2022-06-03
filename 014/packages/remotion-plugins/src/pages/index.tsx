6c90ecc013b72248f7f98d95d19723456bd5da45
JP Calvo <calvojp92@gmail.com>
2022-06-03 21:06:14 +0800

    landing page

--- 014/packages/remotion-plugins/src/pages/index.tsx
+++ 014/packages/remotion-plugins/src/pages/index.tsx
@@ -1,3 +1,28 @@
-export default function Index() {
-  return <div />;
+import {ArrowRightIcon} from "@heroicons/react/outline";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Landing() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <main className="flex grow flex-col items-center justify-center">
+        <div className="mb-8">
+          <Image
+            src="/balloons.svg"
+            alt=""
+            width={300}
+            height={400}
+            draggable={false}
+          />
+        </div>
+
+        <Link href="/create-new" passHref>
+          <a className="flex items-center gap-4 rounded-md border border-gray-200 p-3 px-5 text-sm outline-none transition-all duration-300 hover:border-gray-300 focus:border-blue-400">
+            <span>Create birthday video</span>
+            <ArrowRightIcon className="h-4 w-4" />
+          </a>
+        </Link>
+      </main>
+    </div>
+  );
 }
