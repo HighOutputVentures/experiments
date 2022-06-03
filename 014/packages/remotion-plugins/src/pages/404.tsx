22b7c1dbf3fc096dbf0e395df4115517e3f51715
JP Calvo <calvojp92@gmail.com>
2022-06-03 20:40:51 +0800

    moved 404

---
+++ 014/packages/remotion-plugins/src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-black text-gray-200">
+      <h1 className="text-9xl">404</h1>
+    </div>
+  );
+}
