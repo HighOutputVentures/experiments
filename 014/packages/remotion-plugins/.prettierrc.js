98b3339aa299211d144c5a6b43d5f6d74317445b
JP Calvo <calvojp92@gmail.com>
2022-06-03 19:39:26 +0800

    updated to js

---
+++ 014/packages/remotion-plugins/.prettierrc.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Options} */
+const prettierConfig = {
+  arrowParens: "always",
+  trailingComma: "all",
+  bracketSpacing: false,
+};
+
+module.exports = prettierConfig;
