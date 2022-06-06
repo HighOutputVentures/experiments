0a1eaf799a823307787caafa3a1f7db232e6c8fd
jp calvo <calvojp92@gmail.com>
2022-06-06 17:23:20 +0800

    added title

--- 014/packages/remotion-plugins/src/features/create-new/layout.tsx
+++ 014/packages/remotion-plugins/src/features/create-new/layout.tsx
@@ -1,22 +1,29 @@
 import {ArrowNarrowLeftIcon} from "@heroicons/react/solid";
+import Head from "next/head";
 import Link from "next/link";
 import * as React from "react";
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <div className="p-4">
-        <Link href="/" passHref>
-          <a className="flex w-fit items-center gap-2 text-sm">
-            <ArrowNarrowLeftIcon className="h-4 w-4" />
-            <span>Go back</span>
-          </a>
-        </Link>
-      </div>
+    <>
+      <Head>
+        <title>Create New</title>
+      </Head>
 
-      <main className="flex grow flex-col items-center justify-center">
-        {children}
-      </main>
-    </div>
+      <div className="flex min-h-screen flex-col">
+        <div className="p-4">
+          <Link href="/" passHref>
+            <a className="flex w-fit items-center gap-2 text-sm">
+              <ArrowNarrowLeftIcon className="h-4 w-4" />
+              <span>Go back</span>
+            </a>
+          </Link>
+        </div>
+
+        <main className="flex grow flex-col items-center justify-center">
+          {children}
+        </main>
+      </div>
+    </>
   );
 }
