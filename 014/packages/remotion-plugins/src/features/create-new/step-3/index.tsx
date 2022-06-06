d662a9fffbd9bafb601b9f4212fa93d466ba83ab
jp calvo <calvojp92@gmail.com>
2022-06-06 21:27:43 +0800

    added spinner while processing image

--- 014/packages/remotion-plugins/src/features/create-new/step-3/index.tsx
+++ 014/packages/remotion-plugins/src/features/create-new/step-3/index.tsx
@@ -1,9 +1,10 @@
-import {Player, PlayerRef} from "@remotion/player";
+import {Player} from "@remotion/player";
 import Head from "next/head";
 import {useRouter} from "next/router";
 import * as React from "react";
 import {v4 as uuid} from "uuid";
 import BirthdayCard from "~/components/birthday-card";
+import Spinner from "~/components/spinner";
 import constants from "~/config/constants";
 import useFileToImgSrc from "~/hooks/use-file-to-img-src";
 import dateFormatter from "~/utils/date-formatter";
@@ -13,7 +14,6 @@ import useStore from "../use-store";
 export default function CreateNewStep3() {
   const store = useStore();
   const router = useRouter();
-  const playerRef = React.useRef<PlayerRef>(null);
   const {src: image, loading} = useFileToImgSrc(store.data?.celebrant.image);
 
   React.useEffect(() => {
@@ -22,40 +22,39 @@ export default function CreateNewStep3() {
 
   if (!store.data) return null;
 
-  if (loading) <Loader />;
-
   return (
     <Layout>
       <Head>
-        <title>Happy Birthday, {store.data.celebrant.name}!</title>
+        {loading && <title>Loading...</title>}
+        {!loading && (
+          <title>Happy Birthday, {store.data.celebrant.name}!</title>
+        )}
       </Head>
 
-      <div className="border border-gray-200">
-        <Player
-          ref={playerRef}
-          fps={constants.FPS}
-          durationInFrames={constants.FPS * 30}
-          component={BirthdayCard}
-          compositionWidth={640}
-          compositionHeight={640}
-          controls
-          inputProps={{
-            messages: store.data.messages,
-            celebrant: {
-              id: uuid(),
-              name: store.data.celebrant.name,
-              image,
-              dateOfBirth: dateFormatter.format(
-                store.data.celebrant.dateOfBirth,
-              ),
-            },
-          }}
-        />
-      </div>
+      {loading && <Spinner />}
+      {!loading && (
+        <div className="border border-gray-100">
+          <Player
+            fps={constants.FPS}
+            durationInFrames={constants.FPS * 30}
+            component={BirthdayCard}
+            compositionWidth={640}
+            compositionHeight={640}
+            controls
+            inputProps={{
+              messages: store.data.messages,
+              celebrant: {
+                id: uuid(),
+                name: store.data.celebrant.name,
+                image,
+                dateOfBirth: dateFormatter.format(
+                  store.data.celebrant.dateOfBirth,
+                ),
+              },
+            }}
+          />
+        </div>
+      )}
     </Layout>
   );
-}
-
-function Loader() {
-  return <div className="p-2 text-sm text-gray-400">Loading...</div>;
 }
