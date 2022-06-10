3ae645d03ea2fd2bf3177adea5fcef8bcb928456
jp calvo <calvojp92@gmail.com>
2022-06-10 09:49:21 +0800

    updated outro

--- 014/packages/remotion-plugins/src/components/birthday-card/slide3/index.tsx
+++ 014/packages/remotion-plugins/src/components/birthday-card/slide3/index.tsx
@@ -1,8 +1,11 @@
 import {
   AbsoluteFill,
   Img,
+  interpolate,
   Sequence,
+  spring,
   staticFile,
+  useCurrentFrame,
   useVideoConfig,
 } from "remotion";
 import constants from "../../../config/constants";
@@ -22,10 +25,67 @@ export default function Slide3() {
       from={slide1Duration + slide2Duration}
       durationInFrames={constants.lastSlideDuration * fps}
     >
-      <AbsoluteFill className="items-center justify-center bg-black text-white">
-        <Img src={staticFile("hov-logo.png")} width={80} />
-        <p className="mt-4 text-lg">Thanks for watching!</p>
+      <AbsoluteFill>
+        <Sequence from={0}>
+          <Background />
+        </Sequence>
+        <Sequence from={10}>
+          <Logo />
+        </Sequence>
+        <Sequence from={30}>
+          <Caption />
+        </Sequence>
       </AbsoluteFill>
     </Sequence>
+  );
+}
+
+function Caption() {
+  const frame = useCurrentFrame();
+  const {fps} = useVideoConfig();
+  const marginLeft = spring({frame, fps, from: -100, to: 0});
+
+  return (
+    <p
+      className="absolute left-1/2 top-[55%] -translate-x-1/2 text-lg opacity-75"
+      style={{
+        marginLeft: `${marginLeft}px`,
+      }}
+    >
+      Thanks for watching!!
+    </p>
+  );
+}
+
+function Background() {
+  const frame = useCurrentFrame();
+  const scale = interpolate(frame, [0, 10], [0, 1], {
+    extrapolateLeft: "clamp",
+    extrapolateRight: "clamp",
+  });
+
+  return (
+    <div
+      className="absolute h-full w-full bg-black text-white"
+      style={{
+        transform: `scale(${scale})`,
+      }}
+    />
+  );
+}
+
+function Logo() {
+  const frame = useCurrentFrame();
+  const {fps} = useVideoConfig();
+  const top = spring({frame, fps, from: -5, to: 40});
+
+  return (
+    <Img
+      className="absolute left-1/2 w-[80px] -translate-x-1/2"
+      src={staticFile("hov-logo.png")}
+      style={{
+        top: `${top}%`,
+      }}
+    />
   );
 }
