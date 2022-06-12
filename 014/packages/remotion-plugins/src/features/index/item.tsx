4f949d9802e9fc022111559fb64811b71e864373
jp calvo <calvojp92@gmail.com>
2022-06-12 22:33:15 +0800

    using Link

--- 014/packages/remotion-plugins/src/features/index/item.tsx
+++ 014/packages/remotion-plugins/src/features/index/item.tsx
@@ -1,6 +1,7 @@
 import {CheckIcon, DuplicateIcon} from "@heroicons/react/solid";
 import clsx from "clsx";
 import Image from "next/image";
+import Link from "next/link";
 import * as React from "react";
 import IBirthdayCard from "../../types/birthday-card";
 
@@ -43,31 +44,28 @@ export default function Item({data}: ItemProps) {
   }, []);
 
   return (
-    <a
-      href={`http://localhost:3000/videos/${data.id}`}
-      rel="noreferrer"
-      target="_blank"
-      className="flex items-center gap-2 border border-gray-200 p-4 transition-all duration-300 hover:border-blue-300 hover:ring-2 hover:ring-blue-100 hover:ring-opacity-50"
-    >
-      <div className="relative h-[48px] w-[48px]">
-        <Image src={data.celebrant.image} alt="" layout="fill" />
-      </div>
+    <Link href={`http://localhost:3000/videos/${data.id}`} passHref>
+      <a className="flex items-center gap-2 border border-gray-200 p-4 transition-all duration-300 hover:border-blue-300 hover:ring-2 hover:ring-blue-100 hover:ring-opacity-50">
+        <div className="relative h-[48px] w-[48px]">
+          <Image src={data.celebrant.image} alt="" layout="fill" />
+        </div>
 
-      <div className="grow">
-        <div>John</div>
-        <div className="text-xs text-gray-500">Created 3 days ago</div>
-      </div>
+        <div className="grow">
+          <div>John</div>
+          <div className="text-xs text-gray-500">Created 3 days ago</div>
+        </div>
 
-      <button title="Copy link" onClick={handleClick}>
-        <SVGIcon
-          className={clsx(
-            "h-6 w-6",
-            copied && "fill-green-400",
-            !copied &&
-              "fill-gray-300 transition-all duration-300 hover:fill-gray-400",
-          )}
-        />
-      </button>
-    </a>
+        <button title="Copy link" onClick={handleClick}>
+          <SVGIcon
+            className={clsx(
+              "h-6 w-6",
+              copied && "fill-green-400",
+              !copied &&
+                "fill-gray-300 transition-all duration-300 hover:fill-gray-400",
+            )}
+          />
+        </button>
+      </a>
+    </Link>
   );
 }
