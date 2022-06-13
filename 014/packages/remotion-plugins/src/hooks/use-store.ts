82b5fec55719cb5120a9cf1dfff6c5f0a1f3fcff
jp calvo <calvojp92@gmail.com>
2022-06-13 08:35:40 +0800

    exported store schema

--- 014/packages/remotion-plugins/src/hooks/use-store.ts
+++ 014/packages/remotion-plugins/src/hooks/use-store.ts
@@ -2,7 +2,7 @@ import create from "zustand";
 import {devtools} from "zustand/middleware";
 import {Schema} from "../features/create-new/types";
 
-interface Store {
+export interface Store {
   data?: Schema;
   update: (data: Schema) => void;
   clear: () => void;
