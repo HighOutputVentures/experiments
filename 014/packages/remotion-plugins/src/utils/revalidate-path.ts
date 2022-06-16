fee558357ddcd215406d639fd47b532706cc8d22
jp calvo <calvojp92@gmail.com>
2022-06-16 20:44:22 +0800

    removed revalidate

--- 014/packages/remotion-plugins/src/utils/revalidate-path.ts
+++
@@ -1,9 +0,0 @@
-export default async function revalidatePath(pathname: string) {
-  return await fetch("/api/revalidate", {
-    method: "post",
-    body: JSON.stringify({pathname}),
-    headers: {
-      "Content-type": "application/json",
-    },
-  });
-}
