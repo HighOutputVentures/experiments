7f37e93d57458118e6066b0536d9c71d90ce694c
jp calvo <calvojp92@gmail.com>
2022-06-16 21:01:02 +0800

    added hostname in constants

--- 014/packages/remotion-plugins/src/config/constants.ts
+++ 014/packages/remotion-plugins/src/config/constants.ts
@@ -3,6 +3,7 @@ const slideOneDuration = 10;
 const messageDuration = 5;
 const lastSlideDuration = 5;
 const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+const hostname = process.env.NEXT_PUBLIC_HOSTNAME;
 
 const constants = {
   FPS,
@@ -10,6 +11,7 @@ const constants = {
   messageDuration,
   lastSlideDuration,
   apiEndpoint,
+  hostname,
 };
 
 export default constants;
